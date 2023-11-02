2215f53d4aa499a3536671fb427c368cbb3a3611
Jacob Hoffman <cubhoffman@gmail.com>
2023-11-02 18:14:11 -0400

    ADD: part 1 fixes, probability models, print models, changed token type to chars

--- hw3/main.py
+++ hw3/main.py
@@ -1,7 +1,8 @@
 import argparse
-from collections import Counter
 from n_grams import NGrams
-import os
+import numpy as np
+import pandas as pd
+
 
 data_file_paths = {
     'en': 'datasets/hw3_data/training.en',
@@ -10,27 +11,203 @@ data_file_paths = {
     'test': 'datasets/hw3_data/test'
 }
 
+def convert_probability_model_to_dataframe(arr, index, columns):
+    return pd.DataFrame(arr, index=index, columns=columns)
+
+def save_dataframe_to_file(df, file_name):
+    df.to_csv(file_name, index=True, header=True)
+
+def convert_bigram_tuples_to_strings(tuples_list):
+    bigrams = []
+    for tuple in tuples_list:
+        bigram = tuple[0] + tuple[1]
+        bigrams.append(bigram)
+    return bigrams
+
+def get_unigrams_probabilities(unigrams):
+    probability_model = np.zeros((len(unigrams.vocab)))
+    for i in range(len(unigrams.vocab)):
+        gram = unigrams.vocab[i]
+        num = unigrams.counts[gram]
+        den = unigrams.size
+        probability = num / den
+        probability_model[i] = probability
+
+    if (debug != None):
+        print('unigrams total probability: ', np.sum(probability_model))
+
+    return probability_model
+
+def get_bigrams_probabilities(bigrams, unigrams):
+    vocab_count = len(bigrams.vocab)
+    probability_model = np.zeros((vocab_count, vocab_count))
+    for i in range(vocab_count):
+        word = bigrams.vocab[i]
+        for j in range(len(bigrams.vocab)):
+            bigram = (bigrams.vocab[j], word)
+            num = bigrams.counts[bigram]
+            den = unigrams.counts[word]
+            probability = num / den
+            probability_model[i][j] = probability
+
+    if (debug != None):
+        # print('bigrams total probability', np.sum(probability_model, axis=1))
+        print('bigrams total probabilities summed / vocab_count: ', np.sum(probability_model) / vocab_count)
+
+    return probability_model
+
+def get_trigrams_probabilities(trigrams, bigrams):
+    vocab_count = len(trigrams.vocab)
+    bigram_words = list(bigrams.counts.keys())
+    bigram_words_count = len(bigram_words)
+    probability_model = np.zeros((bigram_words_count, vocab_count))
+    for i in range(vocab_count):
+        word = trigrams.vocab[i]
+        for j in range(bigram_words_count):
+            bigram = bigram_words[j]
+            trigram = bigram + (word,)
+            num = trigrams.counts[trigram]
+            den = bigrams.counts[bigram]
+            probability = num / den
+            probability_model[j][i] = probability
+    
+    if (debug != None):
+        # print('trigrams total probability', np.sum(probability_model, axis=0))
+        print('trigrams total probabilities summed / bigram_words_count: ', np.sum(probability_model) / bigram_words_count)
+    
+    return probability_model
+
+def get_interpolated_bigrams_probabilities(bigrams, unigrams, w1 = 1 / 2, w2 = 1 / 2):
+    vocab_count = len(bigrams.vocab)
+    probability_model = np.zeros((vocab_count, vocab_count))
+    for i in range(vocab_count):
+        word = bigrams.vocab[i]
+        for j in range(len(bigrams.vocab)):
+            bigram = (bigrams.vocab[j], word)
+            bigram_prob = bigrams.counts[bigram] / unigrams.counts[word] * w1
+            unigram_prob = unigrams.counts[word] / unigrams.size * w2
+            probability = bigram_prob + unigram_prob
+            # print('bi_prob', bigram_prob, 'uni_prob', unigram_prob)
+            probability_model[i][j] = probability 
+
+    if (debug != None):
+        # print('bigrams total probability', np.sum(probability_model, axis=1))
+        print('bigrams interpolated total probabilities summed / vocab_count: ', np.sum(probability_model) / vocab_count)
+
+    return probability_model
+
+def get_interpolated_trigrams_probabilities(trigrams, bigrams, unigrams, w1 = 1 / 3, w2 = 1 / 3, w3 = 1 / 3):
+    vocab_count = len(trigrams.vocab)
+    bigram_words = list(bigrams.counts.keys())
+    bigram_words_count = len(bigram_words)
+    probability_model = np.zeros((bigram_words_count, vocab_count))
+    for i in range(vocab_count):
+        word = trigrams.vocab[i]
+        for j in range(bigram_words_count):
+            bigram = bigram_words[j]
+            trigram = bigram + (word,)
+            trigram_prob = trigrams.counts[trigram] / bigrams.counts[bigram] * w1
+            new_bigram = (bigram[1], word)
+            bigram_prob = bigrams.counts[new_bigram] / unigrams.counts[word] * w2
+            unigram_prob = unigrams.counts[word] / unigrams.size * w3
+            # print('tri_prob', trigram_prob, 'bi_prob', bigram_prob, 'uni_prob', unigram_prob)
+            probability = trigram_prob + bigram_prob + unigram_prob
+            probability_model[j][i] = probability
+    
+    if (debug != None):
+        # print('trigrams interpolated total probability', np.sum(probability_model, axis=1))
+        print('trigrams interpolated total probabilities summed / bigram_words_count: ', np.sum(probability_model) / bigram_words_count)
+    
+    return probability_model
+
+# main
+# cli args
 parser = argparse.ArgumentParser()
+parser.add_argument('--words', default=None, action='store_true')
+parser.add_argument('--save', default=None, action='store_true')
+parser.add_argument('--debug', default=None, action='store_true')
 parser.add_argument('--smoothed', default=None, action='store_true')
 parser.add_argument('--interpolation', default=None, action='store_true')                    
 args = parser.parse_args() 
 model_type = 'unsmoothed'
-if(args.smoothed != None):
+debug = None
+save = None
+token_type = 'char'
+if args.words != None:
+    token_type = 'words'
+if args.save != None:
+    save = True
+if args.debug != None:
+    debug = True
+if args.smoothed != None:
     model_type = 'smoothed'
-if(args.interpolation != None):
+if args.interpolation != None:
     model_type = 'interpolation'
 
+
+# get n_gram_models for files
 data_file_path_keys = list(data_file_paths.keys())
 n_gram_models = {}
 for key in data_file_path_keys:
-    if key == 'test':
-        data_file_path = data_file_paths[key]
+    data_file_path = data_file_paths[key]
+    if (debug != None):
         print('current file path: ', data_file_path)
-        f = open(data_file_path)
-        unigrams = NGrams(1, f)
-        bigrams = NGrams(2, f)
-        trigrams = NGrams(3, f) 
-        print('uni**', unigrams.count, len(unigrams.values), unigrams.values)
-        print('bi**', bigrams.count, len(bigrams.values), bigrams.values)
-        print('tri**', trigrams.count, len(trigrams.values), trigrams.values)
+    f = open(data_file_path)
+    unigrams = NGrams(1, f, token_type)
+    bigrams = NGrams(2, f, token_type)
+    trigrams = NGrams(3, f, token_type)
+    n_gram_models[key] = (unigrams, bigrams, trigrams)
+
+# get unigram, bigram, and trigram probabilities for each file
+n_gram_probability_model_dfs = {}
+for key in data_file_path_keys:
+    if key != 'test':
+        unigrams = n_gram_models[key][0]
+        bigrams =  n_gram_models[key][1]
+        trigrams =  n_gram_models[key][2]
+        unigrams_probabilities_model = get_unigrams_probabilities(unigrams)
+        if model_type == 'interpolation':
+            bigrams_probabilities_model = get_interpolated_bigrams_probabilities(bigrams, unigrams)
+            trigrams_probabilities_model = get_interpolated_trigrams_probabilities(trigrams, bigrams, unigrams)   
+        else:
+            bigrams_probabilities_model = get_bigrams_probabilities(bigrams, unigrams)
+            trigrams_probabilities_model = get_trigrams_probabilities(trigrams, bigrams)
+
+        unigrams_prob_model_df = convert_probability_model_to_dataframe(
+            unigrams_probabilities_model,
+            unigrams.vocab,
+            None
+        )
+        bigrams_prob_model_df = convert_probability_model_to_dataframe(
+            bigrams_probabilities_model,
+            bigrams.vocab,
+            bigrams.vocab
+        )
+        trigrams_prob_model_df = convert_probability_model_to_dataframe(
+            trigrams_probabilities_model,
+            convert_bigram_tuples_to_strings(list(bigrams.counts.keys())),
+            trigrams.vocab
+        )
+        n_gram_probability_model_dfs[key] = (
+            unigrams_prob_model_df,
+            bigrams_prob_model_df,
+            trigrams_prob_model_df
+        )
+        if save != None:
+            if (model_type == 'interpolation'):
+                print('saving', key, 'interpolated probabilities models to file')
+                unigram_file_name = 'hw3/' + key + '_unigrams.csv'
+                bigram_file_name = 'hw3/' + key + '_interpolated_bigrams.csv'
+                trigram_file_name = 'hw3/' + key + '_interpolated_trigrams.csv'
+            else:
+                print('saving', key, ' probabilities models to file')
+                unigram_file_name = 'hw3/' + key + '_unigrams.csv'
+                bigram_file_name = 'hw3/' + key + '_bigrams.csv'
+                trigram_file_name = 'hw3/' + key + '_trigrams.csv'
+            save_dataframe_to_file(unigrams_prob_model_df, unigram_file_name)
+            save_dataframe_to_file(bigrams_prob_model_df, bigram_file_name)
+            save_dataframe_to_file(trigrams_prob_model_df, trigram_file_name)
+
+# get perplexity
+
 
--- hw3/n_grams.py
+++ hw3/n_grams.py
@@ -1,30 +1,36 @@
 
 from collections import Counter
 
-def get_n_grams(n, file):
+def get_n_grams(n, file, mode):
     tokens = []
     values = []
-    count = Counter()
     for content in file:
         content = content[:-1] #removing new line character
         content = str.lower(content)
-        content_tokens = content.split()
-        tokens = tokens + content_tokens
-    count = len(tokens) + 1 - n
+        content = '<s> ' + content + ' </s>'
+        if mode == 'char':
+            # chars
+            tokens = tokens + list(content)
+        else:
+            # words
+            content_tokens = content.split()
+            tokens = tokens + content_tokens
+    vocab = list(set(tokens))
+    size = len(tokens) + 1 - n
     if n == 1:
         values = tokens
     else:
-        for i in range(count):
-            value = tuple()
-            for j in range(n):
-                value = value + (tokens[i + j],)
+        for i in range(size):
+            value = tuple(tokens[i : i + n])
             values = values + [value,]
-    return values, count
+    return values, vocab, size, Counter(values)
 
 class NGrams():
     values = []
-    count = Counter() 
-
-    def __init__(self, n, file):
+    vocab = []
+    size = 0
+    counts = Counter()
+    token_type='char'
+    def __init__(self, n, file, token_type='char'):
         file.seek(0)
-        self.values, self.count = get_n_grams(n, file)
+        self.values, self.vocab, self.size, self.counts = get_n_grams(n, file, token_type)
